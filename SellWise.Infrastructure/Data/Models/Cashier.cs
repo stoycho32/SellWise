3ef24c37c2527cff6dce90cb2c7f99b2086c452b
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-05-10 22:01:16 +0300

    Adding cashier which wil inherit default identity user with additional information

---
+++ SellWise.Infrastructure/Data/Models/Cashier.cs
@@ -0,0 +1,37 @@
+﻿using SellWise.Infrastructure.Contracts;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SellWise.Infrastructure.Data.Models
+{
+    public class Cashier : IDisable
+    {
+        public Cashier()
+        {
+            this.IsDisabled = false;
+            this.CashierSales = new List<Sale>();
+            this.CashierShifts = new List<Shift>();
+        }
+
+
+        [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Cashier First Name Must Be Between 2 and 100 Characters Long.")]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Cashier Last Name Must Be Between 2 and 100 Characters Long.")]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        public bool IsDisabled { get; set;}
+
+
+
+
+        //RELATIONS
+        [InverseProperty(nameof(Cashier))]
+        public List<Sale> CashierSales { get; set; }
+        [InverseProperty(nameof(Cashier))]
+        public List<Shift> CashierShifts { get; set; }
+    }
+}
