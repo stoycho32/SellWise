a131ec455c2745641013ff2d0a244d9cb0822d17
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-05-10 22:02:32 +0300

    Sale entity will be the information about which products are sold in the specific sale along with the total amount

---
+++ SellWise.Infrastructure/Data/Models/Sale.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SellWise.Infrastructure.Data.Models
+{
+    public class Sale
+    {
+        public Sale()
+        {
+            this.SaleProducts = new List<SaleProduct>();
+        }
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime SaleDateTime { get; set; }
+
+        [Required]
+        [Precision(18, 2)]
+        public decimal TotalPrice { get; set; }
+
+
+
+        //RELATIONS
+        [Required]
+        public string CashierId { get; set; } = null!;
+        [ForeignKey(nameof(CashierId))]
+        public Cashier Cashier { get; set; } = null!;
+
+        [InverseProperty(nameof(Sale))]
+        public List<SaleProduct> SaleProducts { get; set; }
+    }
+}
