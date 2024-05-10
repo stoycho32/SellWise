10c5e6cdf03e9326f35724ceee39b7fe5740a5af
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-05-10 22:03:58 +0300

    The shift will provide information about all the sales made in the shift along with the cashier that is working the specific shift. Also, the shift will provide the relevant information about the total Turnover for the shift. Updating the application db context by adding all the entities and also making sale product configuration which will allow the mapping table sale product to have a composite key due to being a mapping table.

--- SellWise.Infrastructure/Data/ApplicationDbContext.cs
+++ SellWise.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SellWise.Infrastructure.Data.EntityConfiguration;
+using SellWise.Infrastructure.Data.Models;
 
 namespace SellWise.Data
 {
@@ -11,9 +13,23 @@ namespace SellWise.Data
 
         }
 
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<Cashier> Cashiers { get; set; }
+        public DbSet<Sale> Sales { get; set; } 
+        public DbSet<Shift> Shifts { get; set; }
+        public DbSet<SaleProduct> SaleProducts { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            foreach (var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
 
-
+            builder.ApplyConfiguration(new SaleProductConfiguration());
+            base.OnModelCreating(builder);
+        }
 
     }
 }
