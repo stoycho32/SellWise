d413b17b8c2bbf29bf40b126b2135f2564260199
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-05-10 20:00:42 +0300

    Adding Repository and implementing the logic behind it

---
+++ SellWise.Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SellWise.Infrastructure.Repository
+{
+    public interface IRepository
+    {
+        public IQueryable<T> All<T>();
+
+        public IQueryable<T> AllAsReadOnly<T>();
+
+        public  Task AddAsync<T>(T entity);
+
+        public  Task<T?> GetByIdAsync<T>(object id);
+
+        public  Task<int> SaveChangesAsync();
+    }
+}
