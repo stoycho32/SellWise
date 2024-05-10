2e15282feaa5819beb936687b860b0ada048785f
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-05-10 19:13:29 +0300

    Adding claim principle extension

--- SellWise/Extensions/ClaimPrincipalExtension.cs
+++ SellWise/Extensions/ClaimPrincipalExtension.cs
@@ -2,6 +2,14 @@
 {
     public static class ClaimPrincipalExtension
     {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
 
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRoleName);
+        }
     }
 }
