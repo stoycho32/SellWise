6e1b13e0b69b4bb3ee11c853d77009c7fdb2609c
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-05-15 23:56:26 +0300

    Adding sell controller in order to handle the sales view

---
+++ SellWise/Controllers/SellController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SellWise.Core.Contracts;
+
+namespace SellWise.Controllers
+{
+    public class SellController : Controller
+    {
+        private readonly ISellService productService;
+
+        public SellController(ISellService productService)
+        {
+            this.productService = productService;
+        }
+
+        public IActionResult SellSystem()
+        {
+            return View();
+        }
+    }
+}
