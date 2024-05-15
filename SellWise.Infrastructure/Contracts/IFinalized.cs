91c649900e44ca80fdf4e2c064fb4068bd0e4cf4
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-05-16 00:15:17 +0300

    Adding IFinalized contract in order to implement it in multiple entities. In this case, in shift and sale as it will be needed.

---
+++ SellWise.Infrastructure/Contracts/IFinalized.cs
@@ -0,0 +1,9 @@
+﻿namespace SellWise.Infrastructure.Contracts
+{
+    public interface IFinalized
+    {
+        public bool IsFinalized { get; set; }
+
+        public DateTime? FinalizationDateTime { get; set; }
+    }
+}
