29e2bc654b4368699b2df3bccebe54ed97dfc069
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-05-20 23:04:19 +0300

    Adding product view model in order to create the sell service method for getting all the products

---
+++ SellWise.Core/Models/ProductModel/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace SellWise.Core.Models.ProductModel
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Product Name Must Be Between 2 and 30 characters")]
+        public string ProductName { get; set; } = null!;
+
+        [Required]
+        [Range(1, 50000)]
+        public int ProductQuantity { get; set; }
+
+        [Required]
+        [Precision(18, 2)]
+        [Range(0.01, 15000)]
+        public decimal ProductSellingPrice { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Manufacturer Name Must Be Between 3 and 50 Characters Long")]
+        public string ManufacturerName { get; set; } = null!;
+    }
+}
