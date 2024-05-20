ce3664ba6b4e83ea84406273644ed377b1b1aab0
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-05-21 00:15:03 +0300

    Removing the manufacturer from product view model

--- SellWise.Core/Models/ProductModel/ProductViewModel.cs
+++ SellWise.Core/Models/ProductModel/ProductViewModel.cs
@@ -19,9 +19,5 @@ namespace SellWise.Core.Models.ProductModel
         [Precision(18, 2)]
         [Range(0.01, 15000)]
         public decimal ProductSellingPrice { get; set; }
-
-        [Required]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Manufacturer Name Must Be Between 3 and 50 Characters Long")]
-        public string ManufacturerName { get; set; } = null!;
     }
 }
